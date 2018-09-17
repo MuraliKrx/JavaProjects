1520a09baf9c5ebe8eee150ed6046bda262e65ac
MuraliKrx <hmukrx@gmail.com>
2018-09-17 17:18:32 +0000

    Mysql Trial Changes done

--- MysqlTrial/src/main/java/mysqltrial/mysqltest.java
+++ MysqlTrial/src/main/java/mysqltrial/mysqltest.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//import com.mysql.jdbc.Driver;
+
 /**
  * Servlet implementation class Servclass
  */
@@ -35,10 +35,10 @@ public class mysqltest extends HttpServlet {
     }
     
     public static final String getStatement = "select * from Random";
-    public static final String updateStatement = "update Random set VALUE=? where DATE in (?)";
+    public static final String updateStatement = "update Random set VALUE=?? where VALUE in ([])";
     public static final String insertStatment = "insert into Random (VALUE) values (?)";
-	public static final String deleteStatement = "delete from Random where MINUTE(DATE)%2==0";
-	public static final String selDeleteStatement = "select * from Random where MINUTE(DATE)%2==0";
+	public static final String deleteStatement = "delete from Random where MINUTE(DATE)%2=0";
+	public static final String selDeleteStatement = "select * from Random where MINUTE(DATE)%2=0";
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
@@ -50,13 +50,15 @@ public class mysqltest extends HttpServlet {
 		out.println(" Initial Get : ");
 		out.println(getData());
 		
-		int randomValue = 0;
+		int randomValue = 10;
 		out.println(" Insert Random Value: " + randomValue);
-		insertData(randomValue);
+		String insertStatus = insertData(randomValue);
+		if (insertStatus!=null){out.println(insertStatus);}
 
-        int updateRandomValue = 0; 		
+        int updateRandomValue = randomValue + 5; 		
 		out.println(" Update Random Value: " + updateRandomValue);
-		updateData(randomValue);
+		String updateStatus = updateData(updateRandomValue, randomValue);
+		if (updateStatus!=null){out.println(updateStatus);}
 		
 		out.println(" Final Get : ");
 		out.println(getData());
@@ -80,33 +82,73 @@ public class mysqltest extends HttpServlet {
 	
 	}
 	
-	public void insertData(int randomValue){
-	    String id = insertStatment;
+	public String insertData(int randomValue){
+	 try{
+	 Class.forName("com.mysql.jdbc.Driver").newInstance();;  
+    Connection con=DriverManager.getConnection("jdbc:mysql://db:3306/petclinic","petclinic","password");  
+
+    Statement stmt=con.createStatement();  
+    String insert = insertStatment.replace("?", randomValue+"");
+    stmt.executeUpdate(insert);
+     con.close();  
+    }catch(Exception e){
+        return e.toString();
+    }
+    return null;
+}
+
+	
+	public String updateData(int randomValue, int oldRandomValue){
+	     String update ="";
+	 try{
+	 Class.forName("com.mysql.jdbc.Driver").newInstance();;  
+    Connection con=DriverManager.getConnection("jdbc:mysql://db:3306/petclinic","petclinic","password");  
+
+    Statement stmt=con.createStatement();
+        update = updateStatement.replace("??", randomValue+"");
+        update = update.replace("[]", oldRandomValue+"");
+    stmt.executeUpdate(update);
+    con.close();  
+    }catch(Exception e){return update + " " + e.toString();}
+        return null;
 	}
-	public void updateData(int randomValue){
-	    String id = updateStatement;
-	}
+	
 	public String getData(){
 	
 	 String result = "";
 	 try{
 	 Class.forName("com.mysql.jdbc.Driver").newInstance();;  
-    Connection con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/petclinic","petclinic","password");  
-//here sonoo is database name, root is username and password  
+    Connection con=DriverManager.getConnection("jdbc:mysql://db:3306/petclinic","petclinic","password");  
+
     Statement stmt=con.createStatement();  
     ResultSet rs=stmt.executeQuery(getStatement);
-   while(rs.next()) 
-      result =  rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3) + " \n";  
+   while(rs.next()) {
+      result =  result + "\n" + rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3) + " \n";  
+      }
     con.close();  
     }catch(Exception e){
         result = "Error" + e.getMessage() + e.toString();
-    }   
+    }
 	    return result;
 	}
 	public String deleteData(){
-	    String id = deleteStatement;
-	    String id2 = selDeleteStatement;
-	    return null;
+	  String result = "";
+	 try{
+	 Class.forName("com.mysql.jdbc.Driver").newInstance();;  
+    Connection con=DriverManager.getConnection("jdbc:mysql://db:3306/petclinic","petclinic","password");  
+
+    Statement stmt=con.createStatement();  
+    ResultSet rs=stmt.executeQuery(selDeleteStatement);
+   while(rs.next()) {
+      result =  result + "\n" + rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3) + " \n";  
+      }
+      
+      int status = stmt.executeUpdate(deleteStatement);
+    con.close();  
+    }catch(Exception e){
+        result = "Error" + e.getMessage() + e.toString();
+    }
+	    return result;
 	}
 
 }
